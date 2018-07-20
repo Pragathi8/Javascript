8bdcf18ee30cab872fca2ad241cdb1c84ef4db12
Pragathi Nataraj <pranatar@publicisgroupe.net>
2018-07-20 21:05:10 +0530

    assigment3

---
+++ Javascript/static-web-boilerplate/src/js/index.js
@@ -0,0 +1,6 @@
+const jQuery=require ("jquery");
+import "popper.js";
+import "bootstrap";
+require ("../../node_modules/bootstrap/scss/bootstrap.scss");
+require ("../scss/abc.scss");
+import "./first";
