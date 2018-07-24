df7f5505409c8b42cf59be763602de97c1f2f5bb
Pragathi Nataraj <pranatar@publicisgroupe.net>
2018-07-24 14:34:00 +0530

    new changes

--- Javascript/static-web-boilerplate/src/js/first.js
+++ Javascript/static-web-boilerplate/src/js/first.js
@@ -1,43 +1,8 @@
 const jQuery = require("jquery");
 
 let globalPopularList = [];
-let globalSearchList = [];
 
-document.getElementById("popupId").addEventListener('click', searchResult);
-function searchResult() {
-    const searchMovie = document.getElementById("searchMovie").value;
-    const url = "https://api.themoviedb.org/3/search/movie?api_key=b93f81fd0b7c110be2ee1cbeab848d12&query=" + searchMovie;
-    var popup = "";
-    fetch(url)
-        .then((resp) => resp.json())
-        .then(function (data) {
-            let movies = data.results;
-            console.log(movies);
-            globalPopularList.push(movies);
-            movies.map(function (movie) {
-                var desc=movie.overview;
-                var description=desc.substring(0,150);
-                popup += `<div class="card mb-4 searchMovie">
-              <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Card image cap" id="popup-img">
-              <span class='right'>
-                <button type="button" class="favorite" name="${movie.id}"><i class='fas fa-bookmark'></i></button>
-                <button type="button" class="favorite" name="${movie.id}"><i class='fas fa-heart'></i></button>
-            </span>
-              <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">${description}...</p>
-                <p class="card-text"><small class="text-muted" style="float:right">More Info</small></p>
-              </div>
-            </div>`;
-            })
-            document.getElementsByClassName("modal-body")[0].innerHTML=popup;
-            // document.getElementById("popup-body").innerHTML = popup;
-        })
-        console.log("searchresults:"+globalSearchList);
-}
-
-function loadPage() {
-    const url = "https://api.themoviedb.org/3/movie/popular?api_key=b93f81fd0b7c110be2ee1cbeab848d12&language=en-US&page=1";
+function createCard(url, container) {
     var popular = "";
     fetch(url)
         .then((resp) => resp.json())
@@ -45,29 +10,47 @@ function loadPage() {
             let movies = data.results;
             globalPopularList.push(movies);
             movies.map(function (movie) {
-                var desc=movie.overview;
-                var description=desc.substring(0,150);
+                var desc = movie.overview;
+                var description = desc.substring(0, 150);
                 popular += `<div class="card mb-4 listOfPop">
-                <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Card image cap" id="popup-img">
+                <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Card image cap" class="popup-img">
                 <span class='right'>
                   <button type="button" class="favorite" name="${movie.id}" ><i class='fas fa-bookmark'></i></button>
-                  <button type="button" class="favorite" name="${movie.id}" ><i class='fas fa-heart'></i></button>
-              </span>
+                  <button type="button" class="favorite" id="popupId" name="${movie.id}" data-toggle="modal" data-target="#colmyModal"><i class='fas fa-heart'></i></button> 
+                </span>
             <div class="card-body">
               <h5 class="card-title">${movie.title}</h5>
               <p class="card-text">${description}....</p>
               <p class="card-text"><small class="text-muted" style="float:right">${movie.release_date}</small></p>
             </div>
-          </div>`;
+          </div>
+          <div>
+          </div>
+      </div>
+          </div>
+          `;
             })
-            document.getElementsByClassName("container-2")[0].innerHTML = popular;
-
+            container.innerHTML = popular;
         })
-    console.log(globalPopularList);
 }
-function removeDetails(movieId){
+
+document.getElementById("popupId").addEventListener('click', searchResult);
+function searchResult() {
+    const searchMovie = document.getElementById("searchMovie").value;
+    const url = "https://api.themoviedb.org/3/search/movie?api_key=b93f81fd0b7c110be2ee1cbeab848d12&query=" + searchMovie;
+    var container = document.getElementsByClassName("modal-body")[0];
+    createCard(url, container);
+}
+
+function loadPage() {
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=b93f81fd0b7c110be2ee1cbeab848d12&language=en-US&page=1";
+    var container = document.getElementsByClassName("container-2")[0];
+    createCard(url, container);
+}
+
+function removeDetails(movieId) {
     console.log(movieId);
-    fetch('http://localhost:3000/action/'+movieId, {
+    fetch('http://localhost:3000/action/' + movieId, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json; charset=utf-8",
@@ -77,7 +60,7 @@ function removeDetails(movieId){
 
 function getMovieDetails(movieId) {
     console.log(movieId);
-    console.log("length:"+globalPopularList.length);
+    console.log("length:" + globalPopularList.length);
     for (var i in globalPopularList) {
         var global = globalPopularList[i];
         for (var movie in global) {
@@ -92,6 +75,10 @@ function getMovieDetails(movieId) {
             }
         }
     }
+    addToJsonServer(movieId, title, image, desc, relDate);
+}
+
+function addToJsonServer(movieId, title, image, desc, relDate) {
     console.log("title" + title);
     fetch('http://localhost:3000/action', {
         method: "POST",
@@ -108,21 +95,21 @@ function getMovieDetails(movieId) {
     })
 }
 
-function addToCollections(){
-    const url="http://localhost:3000/action";
+function addToCollections() {
+    const url = "http://localhost:3000/action";
     var collection = "";
     fetch(url)
-    .then((resp) => resp.json())
-    .then(function (data) {
-        let movies = data;
-        console.log(data);
-        movies.map(function (movie) {
-            var desc=movie.overview;
-            var description=desc.substring(0,150);
-            collection += `<div class="card mb-4 listOfPop">
-                <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Card image cap" id="popup-img">
+        .then((resp) => resp.json())
+        .then(function (data) {
+            let movies = data;
+            console.log(data);
+            movies.map(function (movie) {
+                var desc = movie.overview;
+                var description = desc.substring(0, 150);
+                collection += `<div class="card mb-4 listOfPop">
+                <img class="card-img-top" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Card image cap" class="popup-img">
                 <span class='right'>
-                  <button type="button" class="delete" name="${movie.id}" ><i class="fas fa-trash"></i></button>
+                  <button type="button" class="delete" name="${movie.id}" style="float:right"><i class="fas fa-trash"></i></button>
               </span>
             <div class="card-body">
               <h5 class="card-title">${movie.title}</h5>
@@ -136,14 +123,14 @@ function addToCollections(){
 }
 
 function eventListener() {
-    jQuery(document).on("click", ".favorite, .favoriteSearch", function () {
+    jQuery(document).on("click", ".favorite", function () {
         var movieId = this.name;
         console.log(movieId);
         getMovieDetails(movieId);
         addToCollections();
     })
-    jQuery(document).on("click",".delete",function(){
-        var movieId=this.name;
+    jQuery(document).on("click", ".delete", function () {
+        var movieId = this.name;
         removeDetails(movieId);
         addToCollections();
     })
--- Javascript/static-web-boilerplate/src/js/index.js
+++ Javascript/static-web-boilerplate/src/js/index.js
@@ -2,5 +2,5 @@ const jQuery=require ("jquery");
 import "popper.js";
 import "bootstrap";
 require ("../../node_modules/bootstrap/scss/bootstrap.scss");
-require ("../scss/abc.scss");
+require ("../scss/style.scss");
 import "./first";
